fe220021a0cf9a0a54d1acbcd3ef83b8e3120d3f
Salyarket <keenkima@yahoo.com>
2025-09-17 16:07:01 +0200

    Saliha: addind logs handling on project

--- config.ts
+++ config.ts
@@ -4,6 +4,6 @@ export const config = {
     // allowedOrigins: process.env.ALLOWED_ORIGINS || "*",
     // jwtSecret: process.env.JWT_SECRET || "jwt-secret",
     // secure: process.env.NODE_ENV === "production" || false,
-    // logLevel: process.env.LOG_LEVEL || "info",
+    logLevel: process.env.LOG_LEVEL || "info",
   },
 };
