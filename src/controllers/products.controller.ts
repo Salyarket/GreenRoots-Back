9b7097b1f183e37198125a2dd48b742fa0f3a40c
adrien.paris123 <adrien.paris123@gmail.com>
2025-09-17 16:02:22 +0200

    Adrien Pâris / features : controller + ruoter add of create, update, delate of a product

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -19,8 +19,8 @@ import { parseIdFromParams } from "./utils.js";
 
 export async function getAllProducts(req: Request, res: Response) {
 
-//   Query params permettant la gestion de la pagination (limit + page) et le trie Asc ou Desc (optionnel)
-//   exemple de query params => /products?limit=10&page=2&order=name:asc  
+  //   Query params permettant la gestion de la pagination (limit + page) et le trie Asc ou Desc (optionnel)
+  //   exemple de query params => /products?limit=10&page=2&order=name:asc  
 
   const { page, limit, order } = await z.object({
     limit: z.coerce.number().int().min(1).optional().default(8),
@@ -66,3 +66,90 @@ export async function getOneProduct(req: Request, res: Response) {
 
   res.json(product);
 }
+
+
+const transformWith2Decimal = z.number().transform((val) =>
+  Math.round(val * 100) / 100
+//   { message: "Doit avoir au maximum 2 décimales" }
+);
+
+export async function createProduct(req: Request, res: Response) {
+  
+// --! Tester si c'est la longueur du tableau ou d'un élément du tableau que zod check 
+// --! ajouter une fonction qui transforme le nom en slug 
+
+  // Body validation with optional properties
+  const createProductBodySchema = z.object({
+    name: z.string().min(1).max(255),
+    slug: z.string().min(1).max(255),
+    price:  transformWith2Decimal.default(0.0),
+    description: z.string().min(1).max(2500),
+    image_urls: z.array(z.string()).max(100),
+    available: z.boolean().default(true),
+    stock: z.number().int().nonnegative().default(0),
+    scientific_name: z.string().max(255).nullable().optional(),
+    carbon: transformWith2Decimal.nullable().optional(),
+  });
+
+  const { name, slug, price, description, image_urls, available, stock, scientific_name, carbon } = await createProductBodySchema.parseAsync(req.body);
+
+  const product = await prisma.product.create({ data: {
+    name,
+    slug,
+    price,
+    description,
+    image_urls,
+    available,
+    stock,
+    scientific_name,
+    carbon
+  }});
+
+  res.status(201).json(product);
+}
+
+export async function updateProduct(req: Request, res: Response) {
+
+  const productId = await parseIdFromParams(req.params.id);
+
+// --! ajouter une fonction qui transforme le nom en slug si celui-ci est changé
+
+  const { name, slug, price, description, image_urls, available, stock, scientific_name, carbon } = await z.object({
+    name: z.string().min(1).max(255).optional(),
+    slug: z.string().min(1).max(255).optional(),
+    price:  transformWith2Decimal.default(0.0).optional(),
+    description: z.string().min(1).max(2500).optional(),
+    image_urls: z.array(z.string()).max(100).optional(),
+    available: z.boolean().default(true).optional(),
+    stock: z.number().int().nonnegative().default(0).optional(),
+    scientific_name: z.string().max(255).nullable().optional().optional(),
+    carbon: transformWith2Decimal.nullable().optional().optional(),
+  }).parseAsync(req.body);
+
+  const updatedProduct = await prisma.product.update({
+    where: { id: productId },
+    data: {
+      ...(name && { name }),
+      ...(slug && { slug }),
+      ...(price && { price }),
+      ...(description && { description }),
+      ...(image_urls && { image_urls }),
+      ...(available && { available }),
+      ...(stock && { stock }),
+      ...(scientific_name && { scientific_name }),
+      ...(carbon && { carbon }),
+    }
+  });
+
+  // Respond to client
+  res.json(updatedProduct);
+}
+
+
+export async function deleteProduct(req: Request, res: Response) {
+  const productId = await parseIdFromParams(req.params.id);
+
+  await prisma.product.delete({ where: { id: productId } });
+  res.status(204).json();
+}
+
--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -7,6 +7,6 @@ console.log("est-ce que je passe par le router");
 
 router.get("/products", productsController.getAllProducts);
 router.get("/products/:id", productsController.getOneProduct);
-// router.post("/products",  productsController.createProduct);
-// router.patch("/products/:id",  productsController.updateProduct);
-// router.delete("/products/:id",  productsController.deleteProduct);
+router.post("/products",  productsController.createProduct);
+router.patch("/products/:id",  productsController.updateProduct);
+router.delete("/products/:id",  productsController.deleteProduct);
