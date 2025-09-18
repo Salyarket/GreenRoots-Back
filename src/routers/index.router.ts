b14327ddee922393fc885d9ea0ec46449463c78b
Tarig Mekhazni <tmk-dev@hotmail.com>
2025-09-18 14:26:09 +0200

    update redirect

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -4,9 +4,7 @@ import authRouter from "./auth_routes.js";
 export const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to the GREENROOTS API",
-  });
+  res.redirect("/api-docs");
 });
 
 router.use("/auth", authRouter);
