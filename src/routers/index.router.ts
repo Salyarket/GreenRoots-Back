a419f4a14b227f299f5cda79014db8d1b1fe0a4e
Tarig Mekhazni <tmk-dev@hotmail.com>
2025-09-18 10:51:05 +0200

    update on the index.router.ts

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -2,13 +2,6 @@ import { Router } from "express";
 
 export const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-router.get("/toto", (req, res) => {
-  res.json({ data: "toto" });
-});
-router.use("/test", (req, res) => {
-  res.json({ data: "test" });
+router.use("/", (req, res) => {
+  res.send("Welcome on the GreenRoots API");
 });
