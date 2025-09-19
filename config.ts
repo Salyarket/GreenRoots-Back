4b27f738acf9cdfa747aad4185fc7f5e9e323b63
Tarig Mekhazni <tmk-dev@hotmail.com>
2025-09-19 11:39:33 +0200

    update on the port : 4000

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const config = {
   server: {
-    port: parseInt(process.env.PORT || "3333"),
+    port: parseInt(process.env.PORT || "4444"),
     allowedOrigins: process.env.ALLOWED_ORIGINS || "*",
     jwtSecret: process.env.JWT_SECRET || "jwt-secret",
     secure: process.env.NODE_ENV === "production" || false,
