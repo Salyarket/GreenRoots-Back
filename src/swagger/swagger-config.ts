ff05f00ccd1c46ba3778dc0d7d6b7b8dc688394c
Oumaima-afk <oumaima_afakkir@hotmail.com>
2025-09-22 14:00:45 +0200

    Oumaima: fix app and swagger-config files

--- src/swagger/swagger-config.ts
+++ src/swagger/swagger-config.ts
@@ -69,10 +69,8 @@ const options: swaggerJsdoc.Options = {
           required: ["code", "label", "tva_rate"],
         },
       },
-    },
-    components:{
-      securitySchemes:{
-        bearerAuth:{
+      securitySchemes: {
+        bearerAuth: {
           type: "http",
           scheme: "bearer",
           bearerFormat: "JWT",
