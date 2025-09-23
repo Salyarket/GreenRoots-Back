e1be7c7c8cf0c641fc5810f6777242903acbb8be
Oumaima-afk <oumaima_afakkir@hotmail.com>
2025-09-23 15:41:27 +0200

    fix import

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,32 +1,30 @@
 import { PrismaClient } from "@prisma/client";
-import BaseController from "./base.controllers.js";
+import BaseController from "./base.controller.js";
 import { parseIdFromParams } from "../utils/zod.js";
 import { Pagination } from "../utils/pagination.js";
-import { productDbSchema, 
-  productSchemaForCreate,  
-  productSchemaForUpdate} 
-  from "../schemas/product.schema.js";
-import { ImgCreatePath, 
-  ImgUpdatePath, 
-  deleteFiles} 
-  from "../utils/imgUpload.js";
-import { TextToSlugCreate, 
-  TextToSlugUpdate
-} from "../utils/TextToSlug.js";
+import {
+  productDbSchema,
+  productSchemaForCreate,
+  productSchemaForUpdate,
+} from "../schemas/product.schema.js";
+import {
+  ImgCreatePath,
+  ImgUpdatePath,
+  deleteFiles,
+} from "../utils/imgUpload.js";
+import { TextToSlugCreate, TextToSlugUpdate } from "../utils/TextToSlug.js";
 
 const prisma = new PrismaClient();
 
-
 class ProductController extends BaseController {
   constructor() {
-    super(prisma.product, "product", productDbSchema );
+    super(prisma.product, "product", productDbSchema);
   }
 
-  // Pour tester avec postman : 
+  // Pour tester avec postman :
   // GET http://localhost:3000/products/pagination?limit=5&page=2&order=name:desc
   // GET http://localhost:3000/products/pagination?limit=5&page=2&order=name:asc
   getProductsWithPagination = async (req: Request, res: Response) => {
-
     // Reste à faire : Les filtres par prix, localisation, carbon ...
 
     try {
@@ -35,9 +33,7 @@ class ProductController extends BaseController {
       const products = await prisma.product.findMany({
         skip: (page - 1) * limit,
         take: limit,
-        orderBy: order === "name:asc" 
-          ? { name: "asc" }
-          : { name: "desc" },
+        orderBy: order === "name:asc" ? { name: "asc" } : { name: "desc" },
       });
 
       const total = await prisma.product.count();
@@ -52,18 +48,16 @@ class ProductController extends BaseController {
         },
       });
     } catch (error) {
-      return res.status(400).json({ 
-        error: "Paramètres de recherche non valide", 
-        details: error 
+      return res.status(400).json({
+        error: "Paramètres de recherche non valide",
+        details: error,
       });
     }
-
   };
 
   getOneProductWithLocations = async (req: any, res: any) => {
-
     const productId = await parseIdFromParams(req.params.id);
-    
+
     const product = await prisma.product.findUnique({
       where: { id: productId },
       include: {
@@ -74,19 +68,18 @@ class ProductController extends BaseController {
                 id: true,
                 name: true,
                 latitude: true,
-                longitude: true
-              }
-            }
-          }
-        }
-      }
+                longitude: true,
+              },
+            },
+          },
+        },
+      },
     });
-    
+
     res.json(product);
   };
 
   createProduct = async (req: any, res: any) => {
-
     try {
       // 1. Validation des données brutes
       const data = await productSchemaForCreate.parseAsync(req.body);
@@ -94,10 +87,13 @@ class ProductController extends BaseController {
       // 2. Création du slug à partir du nom du produit
       const dataWithSlug = TextToSlugCreate(data);
 
-      // 3. Ajout des chemins pour accéder aux images 
-      const imgPath = ImgCreatePath(dataWithSlug, req.files as Express.Multer.File[]);
+      // 3. Ajout des chemins pour accéder aux images
+      const imgPath = ImgCreatePath(
+        dataWithSlug,
+        req.files as Express.Multer.File[]
+      );
 
-      // 4. Vérification du nombre d’images 
+      // 4. Vérification du nombre d’images
       if (imgPath.image_urls.length > 3) {
         return res
           .status(400)
@@ -110,13 +106,12 @@ class ProductController extends BaseController {
     } catch (error) {
       console.error(error);
       res.status(400).json({ error: "données invalides" });
-    }    
+    }
   };
 
   updateProduct = async (req: any, res: any) => {
     try {
-
-      // 1. Vérifier que l'id est bien un nombre 
+      // 1. Vérifier que l'id est bien un nombre
       const productId = Number(req.params.id);
       if (isNaN(productId)) {
         return res.status(400).json({ error: "Id du produit invalide" });
@@ -137,7 +132,11 @@ class ProductController extends BaseController {
       const dataWithSlug = TextToSlugUpdate(existingProduct, data);
 
       // 5. Remplacer ou ajouter des images
-      const dataWithImg = ImgUpdatePath(existingProduct , dataWithSlug, req.files as Express.Multer.File[]);
+      const dataWithImg = ImgUpdatePath(
+        existingProduct,
+        dataWithSlug,
+        req.files as Express.Multer.File[]
+      );
 
       const updatedProduct = await prisma.product.update({
         where: { id: productId },
@@ -145,7 +144,6 @@ class ProductController extends BaseController {
       });
 
       res.status(200).json(updatedProduct);
-
     } catch (error: any) {
       if (error.name === "ZodError") {
         return res.status(400).json(error.issues);
@@ -163,7 +161,7 @@ class ProductController extends BaseController {
 
   deleteProduct = async (req: any, res: any) => {
     try {
-      // 1. Vérifier que l'id est bien un nombre 
+      // 1. Vérifier que l'id est bien un nombre
       const productId = Number(req.params.id);
       if (isNaN(productId)) {
         return res.status(400).json({ error: "Id du produit invalide" });
@@ -182,10 +180,13 @@ class ProductController extends BaseController {
       // 3. Supprimer les fichiers images associés
       if (product.image_urls?.length) {
         try {
-          await deleteFiles(product.image_urls); 
+          await deleteFiles(product.image_urls);
         } catch (fileError) {
-          console.error("impossible de supprimer le ou les fichiers:", fileError);
-        // Attention : erreur indiqué mais on continue la suppression en DB (a voir si return)
+          console.error(
+            "impossible de supprimer le ou les fichiers:",
+            fileError
+          );
+          // Attention : erreur indiqué mais on continue la suppression en DB (a voir si return)
         }
       }
 
@@ -193,7 +194,7 @@ class ProductController extends BaseController {
         where: { id: productId },
       });
 
-      // Réponse : succès mais pas de contenu à favoriser en production 
+      // Réponse : succès mais pas de contenu à favoriser en production
       // res.status(204).send();
       res.status(200).json({
         message: "Le produit a bien été supprimé avec ses images",
@@ -207,7 +208,6 @@ class ProductController extends BaseController {
       res.status(500).json({ error: "Erreur Serveur" });
     }
   };
-
 }
 
 export default new ProductController();
